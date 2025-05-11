a7bbc52a25f60bb18579d301371d90206534c269
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-05-11 22:04:48 +0500

    VITE_API_URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'https://airline-system-pbjy.onrender.com/api'
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE,
